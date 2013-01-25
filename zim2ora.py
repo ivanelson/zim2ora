b504a3ba38cdffcfeebf25aa266e40a71282ea40
Ivan Nunes <ivan@armazemparaiba.com.br>
2013-01-25 19:50:47 -0300

    Stable version...

--- zim2ora.py
+++ zim2ora.py
@@ -1,21 +1,63 @@
 #!/usr/bin/env python
 #-*- encoding: utf-8 -*-
 # Exporta Dados de Arquivos CSV Para o Oracle
-# * Campos de Data Devem ser validados antes de ser importados.
+# * Par 
 
 import cx_Oracle as dbatez
 import sys, csv
 from decimal import Decimal
 from datetime import datetime
 
-def validFields(inFile, tableName, table_columns):
-    L = []
-    #fields = \
-    #[('NR_CRM', None, None, 'VARCHAR2','Y'),('ID',None, None,'VARCHAR2','Y')]
-    #fileSales='contrato.csv' 
-#    for i in table_columns: print i 
+def read_columns(cursor, tablename):
+    vSQL = '''SELECT cols.column_name
+                 FROM all_constraints cons, all_cons_columns cols
+                 WHERE cols.table_name = '{0}'
+                 AND cons.constraint_type = 'P'
+                 AND cons.constraint_name = cols.constraint_name
+                 AND cols.owner='DBATEZ'
+                 AND cons.owner = cols.owner
+            '''.format(tablename)
+    cursor.execute(vSQL)
+    pKey = cursor.fetchone()[0]
 
+    vSQL = '''
+       SELECT COLUMN_NAME, DATA_PRECISION, DATA_SCALE, DATA_TYPE, NULLABLE
+              FROM USER_TAB_COLUMNS
+              WHERE TABLE_NAME='{0}' ORDER BY COLUMN_ID 
+           '''.format(tablename)
+    cursor.execute(vSQL)     # Retrieve columns
+    columns = cursor.fetchall()
+    return (pKey, columns) 
+
+def create_tabletemp(cursor, tablename):
+                          # Check Table if exists  
+    vSQL='''SELECT  TABLE_NAME FROM USER_TABLES
+                           WHERE TABLE_NAME = '{0}_TEMP'
+         '''.format(tablename)
+    cursor.execute(vSQL)
+    if not cursor.fetchone():
+        vSQL='''CREATE GLOBAL TEMPORARY TABLE {0}_TEMP AS
+               (SELECT * FROM {1} WHERE 1=2)
+             '''.format(tablename, tablename)
+        cursor.execute(vSQL)
+        print '%s_TEMP TEMPORARY TABLE HAS BEEN CREATED.' % tablename
+
+def validFields(inFile, tableName, table_columns, cursor):
+    """
+    table_columns -> 
+    [('NR_CRM', None, None, 'VARCHAR2','Y'),('ID',None, None,'VARCHAR2','Y')]
+    """
+    L = []
     columns = [ col[0] for col in table_columns ]
+
+    sSQL = ('INSERT INTO {0}_TEMP (%s) VALUES (%s)'.format(tableName) %
+           (','.join('%s' % name[0] for name in table_columns),
+             ','.join(
+                       "NVL(:{0},null)".format(i + 1) \
+                       if str(value[3])=='DATE' else \
+                          ':{0}'.format(i + 1)  \
+                       for i, value in enumerate(list(table_columns)) )))
+
     with open(inFile, 'rb') as f:
        reader = csv.reader(f, delimiter=';', quotechar='"')
        for row in reader:
@@ -26,65 +68,27 @@ def validFields(inFile, tableName, table_columns):
                          if table_columns[pos][3] == 'VARCHAR2':
                              row[pos] = ' '
                          elif table_columns[pos][3] == 'DATE':
-                                     row[pos] = None 
-                         elif table_columns[pos][3] == 'NUMBER': 
-                                     row[pos] = 0
+                                     row[pos] = None
+                         elif table_columns[pos][3] == 'NUMBER':
+                                     row[pos] = Decimal(0)
                elif table_columns[pos][3] == 'NUMBER' and \
                        value[1] in (None, '',' '):
-                           #del columns[pos]
-                           #del row[pos]
-                           row[pos] = 0
+                           row[pos] = Decimal(0)
                elif table_columns[pos][3] == 'NUMBER' and \
                        table_columns[pos][2] > 0 and      \
                        value[1] not in ('',' ',None):
-                           #print 'POS: ',value[0]
-                           #print 'VALUE: ',value[1]
-                           #print 'FIELD: ',columns[pos]
-                           row[pos] = float(row[pos])
+                           row[pos] = Decimal(row[pos])
                elif table_columns[pos][3] == 'DATE' and \
                        value[1] not in (None, 0, '0','',' '): # Invalid date
                    try:
                        datetime(value[1])
                    except TypeError:
-                       #row[pos] = 'null'
-                       #print 'Opa! INVALID DATE, no Field %s Type is DATE: %s' \
-                       #     % (table_columns[pos][0],  value[1])
-                       #del columns[pos]
-                       #del row[pos]
                        row[pos] = None
 
                elif table_columns[pos][3] == 'DATE' and \
                      value[1] in (None,0,'0','',' '):
-                         #row[pos] = 'null'
-                         #print 'Opa!DATE is NULL, no Field %s Type is DATE: %s'\
-                         # % (table_columns[pos][0],  value[1])
-                         #del columns[pos]
-                         #del row[pos]
                          row[pos] = None
-           L.append(tuple(row))
-    #sSQL = 'INSERT INTO {0}_TEMP ({1}) VALUES (:{2}) '.format(tableName, \
-    #         ','.join(col for col in columns), \
-    #         ','.join(str(col+1) for col in range(len(columns))))
-    #
-    #print L
-    #sSQL = ('INSERT INTO {0}_TEMP (%s) VALUES (%s)'.format(tableName) %
-    #       (','.join('%s' % name for name in columns),
-    #       ','.join(':{0}'.format(conta + 1) for conta in range(len(columns)))))
-    sSQL = ('INSERT INTO {0}_TEMP (%s) VALUES (%s)'.format(tableName) %
-           (','.join('%s' % name[0] for name in table_columns),
-             ','.join(
-                       "NVL(:{0},null)".format(i + 1) \
-                       if str(value[3])=='DATE' else \
-                          ':{0}'.format(i + 1)  \
-                       for i, value in enumerate(list(table_columns)) )))
-    print sSQL
-    return (sSQL, L)
-
-"""
-reader = csv.reader(open(infile), delimiter=';', quotechar='"')
-for row in reader:
-	L.append(tuple(row))
-"""
+           cursor.execute(sSQL , tuple(row))
 
 def mergeTable(tableName, pKey, table_columns):
     vSQL = '''MERGE INTO {0} TAB_A
@@ -108,7 +112,7 @@ def mergeTable(tableName, pKey, table_columns):
                     INSERT ( \n'''
 
     conta = 0
-    for column in table_columns:     # INSERT
+    for column in table_columns:     # INSERT - COLUMNS
         conta += 1
         if conta <> totalFields:
             vSQL += '     %s ,\n ' % (column[0])
@@ -117,7 +121,7 @@ def mergeTable(tableName, pKey, table_columns):
 
     conta = 0
     vSQL += ''' VALUES ( '''
-    for column in table_columns:     # INSERT
+    for column in table_columns:     # INSERT - VALUES
         conta += 1
         if conta <> totalFields:
             vSQL += '     TAB_B.%s,\n ' % (column[0])
@@ -125,7 +129,6 @@ def mergeTable(tableName, pKey, table_columns):
             vSQL += '     TAB_B.%s ) '  % (column[0])
 
     return vSQL
-
 
 if __name__ == '__main__':
     vTable   = sys.argv[1]  # Table Name in Oracle
@@ -135,19 +138,21 @@ if __name__ == '__main__':
     host     = sys.argv[5]
     db = dbatez.connect("{}/{}@{}/orcl".format(user, password, host))
     cursor = db.cursor()
+    """
                           # Check Table if exists  
     vSQL='''SELECT  TABLE_NAME FROM USER_TABLES
                            WHERE TABLE_NAME = '{0}_TEMP'
          '''.format(vTable)
     cursor.execute(vSQL)
-    if bool(cursor.fetchone()):
-        pass
-    else:
+    if not cursor.fetchone():
         vSQL='''CREATE GLOBAL TEMPORARY TABLE {0}_TEMP AS
                (SELECT * FROM {1} WHERE 1=2)
              '''.format(vTable, vTable)
         cursor.execute(vSQL)
-
+        print '%s_TEMP TEMPORARY TABLE HAS BEEN CREATED.' % vTable
+    """
+    create_tabletemp(cursor, vTable)
+    """
     vKey=None          # Primary Key
     vSQL = '''SELECT cols.column_name
                  FROM all_constraints cons, all_cons_columns cols
@@ -160,48 +165,22 @@ if __name__ == '__main__':
 
     cursor.execute(vSQL)
     vKey = cursor.fetchone()[0]
+    """
+    (vKey, table_columns) = read_columns(cursor, vTable)
 
-
+    """
     vSQL = '''
        SELECT COLUMN_NAME, DATA_PRECISION, DATA_SCALE, DATA_TYPE, NULLABLE
               FROM USER_TAB_COLUMNS
-              WHERE TABLE_NAME='{0}' ORDER BY COLUMN_ID 
+              WHERE TABLE_NAME='{0}' ORDER BY COLUMN_ID
            '''.format(vTable)
     cursor.execute(vSQL)
-    table_columns = cursor.fetchall()
+    table_columns = cursor.fetchall() # Retrieve columns
+    """
 
-    vSQL = mergeTable(vTable, vKey, table_columns)
-
-    (sql_insert, L) = validFields(infile, vTable, table_columns)
-
-    #print sql_insert
-    #print ' '
-    #print vSQL
-    #print ' '
-    #print L
-
-    cursor.prepare(sql_insert)
-    cursor.executemany(sql_insert, L)
-    cursor.execute(vSQL)              # Merge
+    vSQL = mergeTable(vTable, vKey, table_columns)     # Prepare Statement Merge
+    validFields(infile, vTable, table_columns, cursor) # Insert in Table Temp
+    cursor.execute(vSQL)                               # Merge
     db.commit()
     cursor.close()
     db.close()
-
-    #L = []
-    #sql_insert = ('INSERT INTO {0}_TEMP (%s) VALUES (%s)'.format(vTable) %
-    #      (','.join('%s' % name for name in table_columns),
-    #       ','.join("TO_NUMBER(:{0},'99999999.99')".format(conta + 1) if str(coluna[conta][1]).isdigit() else 
-    # "TO_DATE(:{0})".format(conta + 1) if str(coluna[conta][3])=='DATE' else
-    # ':{0}'.format(conta + 1) for conta in range(len(table_columns)))))
-
-
-    #reader = csv.reader(open(infile), delimiter=';', quotechar='"')
-    #for row in reader:
-    #    L.append(tuple(row))
-
-    #cursor.prepare(sql_insert)
-    #cursor.executemany(sql_insert, L)
-    #cursor.execute(vSQL)
-    #db.commit()
-    #cursor.close()
-    #db.close()
